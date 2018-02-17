25fadd3731e78b4dd6c0cc083fdf5e5ce2c351fa
Shubham Bansal <shubhibans@hotmail.com>
2018-02-18 00:20:09 +0530

    1st Model added.

---
+++ StartupApp/Models/Difficulty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartupApp.Models
+{
+    public class Difficulty
+    {
+        
+        public string Difficulty_Level { get; set; }
+
+        public QuestionType QuestionType { get; set; }
+
+        public string QuestionTypeType { get; set; }
+    }
+}
---
+++ StartupApp/Models/QuestionType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartupApp.Models
+{
+    public class QuestionType
+    {
+        public string Type { get; set; }
+
+        public ICollection<Difficulty> Difficulty_Levels { get; set; }
+
+        public QuestionType()
+        {
+            Difficulty_Levels = new Collection<Difficulty>();
+        }
+
+    }
+}
